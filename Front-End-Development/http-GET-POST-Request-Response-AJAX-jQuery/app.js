38c33b070b11123b04a75fdf86bab0d1a675c720
repositoryw <repositoryw@gmail.com>
2019-11-04 20:57:51 +0400

    update-- 04.11.2019

--- Front-End-Development/http-GET-POST-Request-Response-AJAX-jQuery/app.js
+++ Front-End-Development/http-GET-POST-Request-Response-AJAX-jQuery/app.js
@@ -7,11 +7,16 @@ $(document).ready(function() {
   $fname = $("#fname");
   $lname = $("#lname");
 
+  var template =
+    "firstname: {{first_name}}, lastname: {{last_name}} </br> <button  data-id='{{id}}' class='remove'> Delete</button>";
   // append to div  -- DRY
   function addUser(value) {
-    $data.append(
-      "Fname: " + value.first_name + " | Lname: " + value.last_name + "<br/>"
-    );
+    // $data.append(
+    //   "Fname: " + value.first_name + " | Lname: " + value.last_name + "<br/>"
+    // );
+
+    //Mustache JS
+    $data.append(Mustache.render(template, value));
   }
 
   // -------------------------------------- GET ---------------------------------------------------------------
@@ -54,4 +59,24 @@ $(document).ready(function() {
       }
     });
   });
+
+  // -------------------------------------- DELETE ------------------------------------------------
+
+  $data.delegate(".remove", "click", function() {
+    var $div = $(this).closest("div");
+    $.ajax({
+      type: "DELETE",
+      url: "https://reqres.in/api/users/" + $(this).attr("data-id"),
+      success: function() {
+        $div.remove();
+      }
+    });
+  });
 });
+
+// $.ajax({
+//   type: "DELETE",
+//   url: 'url: "https://reqres.in/api/users'
+// });
+
+// -------------------------------------- PUT ------------------------------------------------
